d392c2eed0cc3a244075def6c88d227787dfa092
Akhil Nair <akhilgnair2006@gmail.com>
2026-07-15 19:17:47 +0530

    fixed the link mismatch

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://secure-scan-frontend.onrender.com/api'
+  origin: 'https://secure-scan-frontend.onrender.com'
 }));
 
 //simply backend testing
